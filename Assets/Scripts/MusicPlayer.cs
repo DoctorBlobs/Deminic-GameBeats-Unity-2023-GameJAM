54b7faa59b6822f1674fa43b83968280b50df33b
Doctor-blob <jetze.hettema@coderclass.nl>
2023-07-14 16:54:59 +0200

    I wanna kill myself

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,39 +1,145 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
     public float BPM;
     public float musicLength;
-    public GameObject note;
-    bool[,] noteSlots = {{true, false, false, false, true}, {true, false, false, false, true}};
+    public GameObject jArrow;
+    public GameObject kArrow;
+    public GameObject fArrow;
+    public GameObject dArrow;
+    public GameObject audiosource;
+    int currentSong;
+
+    readonly bool[,] noteSlots = {{true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false,
+ false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false,
+ true, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false,
+ true, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, true, false, false, false, true, false, false, false, false, false, false, false}, {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false,
+ false, false, false, false, false, false, true, false, false, false, false, false, true, false, false, false,
+ false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false,
+ false, false, false, true, false, false, false, false, false, false, true, false, true, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, true, false, true, false}, {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false,
+ false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, true, false, false, false, false, false, true, false,
+ true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,}};
+
+    readonly bool[,] noteSlots2 = {{true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, false, false, false, false, false, false, true, false, true, false, false, false,
+ true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, true, false, false, false, false, true, false, false, true, false, false, false}, {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, true, false, false, false, true, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, true, false, false, false, true, false,
+ false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, true, false, false, true, false, false, false, false, false, false, false, false, false,
+ false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false}, {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, true, false, false, false, true, false, false, false, true, false, false, false, false, false,
+ false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false,
+ false, false, false, false, true, false, false, false, false, false, false, false, false, false, true, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ true, false, false, false, false, false, true, false, false, false, false, false, true, false, false, false,
+ false, false, true, false, false, true, false, false, false, true, false, false, true, false, true, false,
+ false, false, false, false, false, false, true, false, true, false, false, true, false, false, false, false}, {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false,
+ false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, true, false, false, false, true, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false,
+ false, false, false, false, false, false, false, false, true, false, false, true, false, false, false, false,
+ false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false}};
     int currentBeat;
     float lastUpdate;
 
     // Start is called before the first frame update
     void Start()
     {
+        musicLength = 12 * 16;
         BPM = 100;
+        currentBeat = 0;
+        currentSong = 0;
     }
-        // if (Time.realtimeSinceStartup > spawnTime) // TODO move into music handler something
+    // if (Time.realtimeSinceStartup > spawnTime) // TODO move into music handler something
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time - lastUpdate >= BPM / 60)
+        if (currentBeat >= musicLength - 1)
         {
-            currentBeat ++;
-            if (noteSlots[0, currentBeat] == true)
+            currentSong++;
+            currentBeat = 0;
+            BPM = 115;
+        }
+        if (Time.time - lastUpdate >= (60 / BPM) / 4.1)
+        {
+            switch (currentSong)
             {
-                Instantiate(note, new Vector3(1f, 4f, 0f), Quaternion.identity);
+                case 0:
+                if (noteSlots[0, currentBeat] == true)
+                {
+                    Instantiate(jArrow, new Vector3(1f, 4f, 0f), Quaternion.identity);
+                }
+                if (noteSlots[1, currentBeat] == true)
+                {
+                    Instantiate(kArrow, new Vector3(3f, 4f, 0f), Quaternion.identity);
+                }
+                if (noteSlots[2, currentBeat] == true)
+                {
+                    Instantiate(fArrow, new Vector3(-1f, 4f, 0f), Quaternion.identity);
+                }
+                    break;
+                case 1:
+                if (noteSlots2[0, currentBeat] == true)
+                {
+                    Instantiate(jArrow, new Vector3(1f, 4f, 0f), Quaternion.identity);
+                }
+                if (noteSlots2[1, currentBeat] == true)
+                {
+                    Instantiate(kArrow, new Vector3(3f, 4f, 0f), Quaternion.identity);
+                }
+                if (noteSlots2[2, currentBeat] == true)
+                {
+                    Instantiate(fArrow, new Vector3(-1f, 4f, 0f), Quaternion.identity);
+                }
+                if (noteSlots2[3, currentBeat] == true)
+                {
+                    Instantiate(dArrow, new Vector3(-3f, 4f, 0f), Quaternion.identity);
+                }
+                    break;
             }
-            if (noteSlots[1, currentBeat] == true)
-            {
-                Instantiate(note, new Vector3(3f, 4f, 0f), Quaternion.identity);
-            }
+            currentBeat++;
 
-        lastUpdate = Time.time;
+            lastUpdate = Time.time;
         }
     }
 }
--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -10,24 +10,35 @@ public class NoteScroller : MonoBehaviour
     public float scrollMultiplier;
     public float travelDistance;
     public float startYPos;
-    float spawnTime;
     float endTime;
     public bool moveHasStarted;
     public float errorMargin;
     float timeSinceStartup;
-
+    public char whichKey;
+    public KeyCode keyCode;
+    public int score;
     // Start is called before the first frame update
     void Start()
     {
         // spawnTime = 2f;
         startYPos = 4f;
-        travelDistance = startYPos + 3.5f;
-        transform.position = new Vector3(0f, startYPos, 0f);
-        scrollMultiplier = 2;
+        travelDistance = startYPos + 3.7f;
+        scrollMultiplier = 2f;
         noteSpeed = BPM/60f;
         endTime = travelDistance / (noteSpeed * 2);
         timeSinceStartup = 0f;
-        errorMargin = 0.15f;
+        errorMargin = 0.13f;
+        switch (whichKey)
+        {
+            case 'j':
+                keyCode = KeyCode.J; break;
+            case 'k':
+                keyCode = KeyCode.K; break;
+            case 'f':
+                keyCode = KeyCode.F; break;
+            case 'd':
+                keyCode = KeyCode.D; break;
+        }
     }   
 
     void Move()
@@ -39,22 +50,25 @@ public class NoteScroller : MonoBehaviour
     void Update()
     {
         Move();
-        hitNote();
+        HitNote();
         timeSinceStartup += Time.deltaTime;
     }
 
-    void hitNote()
+    void HitNote()
     {
-        if ((timeSinceStartup >= endTime - errorMargin) && (timeSinceStartup <= endTime + errorMargin))
+        if ((timeSinceStartup >= endTime - (2 * errorMargin)) && (timeSinceStartup <= endTime + errorMargin))
         {
-            if (Input.GetKeyDown(KeyCode.J))
+            Debug.Log("ddsaf" + timeSinceStartup);
+            if (Input.GetKeyDown(keyCode))
             {
                 GameObject.Destroy(gameObject);
+                score++;
             }
         }
-        else if (!(timeSinceStartup <= endTime + errorMargin))
+        else if (!(timeSinceStartup <= endTime + 2))
         {
             GameObject.Destroy(gameObject);
+            score--;
         }
     }
 }
